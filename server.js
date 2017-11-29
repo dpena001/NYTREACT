0d93ef19892a3a6b6b4859977f72443d1c49a44a
dpena001 <david.pena@vitcomp.com>
2017-11-29 16:52:09 -0500

    Modificando Server.js 13

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json());
 
-app.use(express.static("./client/public"));
+app.use(express.static("client/build"));
 
 // Add routes, both API and view
 app.use(routes);
