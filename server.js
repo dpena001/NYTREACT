7284101e5c44c8c3214d91927c3e161434f5f434
dpena001 <david.pena@vitcomp.com>
2017-11-29 14:51:27 -0500

    Modificando Server.js 8

--- server.js
+++ server.js
@@ -1,43 +1,37 @@
+// Dependencies
 const express = require("express");
-//const methodOverride = require("method-override");
+const methodOverride = require("method-override");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const routes = require("./routes");
 const path = require("path");
+const routes = require("./routes");
 const app = express();
+
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 
 
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-app.use(express.static(path.resolve(__dirname,'client')));
-
-//app.use(bodyParser.json());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json());
-//app.use(express.static('client/build'));
+app.use(express.static('app/client/build'));
 
-//app.use(express.static(path.join(__dirname,"public")));
-//app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(bodyParser.json());
-//app.use(bodyParser.text());
-//app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static('public'));
+app.use(methodOverride("_method"));
 
 
 
 // Add routes, both API and view
 app.use(routes);
 
-app.get('*', function (req, res) {
-  const index = path.join(__dirname, 'build', 'index.html');
-  res.sendFile(index);
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nytreact";
 
 // Set up promises with mongoose
 mongoose.Promise = Promise;
-
 // Connect to the Mongo DB
 mongoose.connect(MONGODB_URI, {
   useMongoClient: true
