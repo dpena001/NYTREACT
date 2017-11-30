90f82f29e5ebc99058235fe3748dd93b814f1b8b
dpena001 <david.pena@vitcomp.com>
2017-11-30 06:57:47 -0500

    New version 2

--- routes/articles.js
+++ routes/articles.js
@@ -1,19 +1,20 @@
 const router = require("express").Router();
 const articlesController = require("../controllers/articlesController");
 
-//router.route("/")
-//  .get(articlesController.findAll)
-//  .post(articlesController.create);
+
+router.route("/")
+  .get(articlesController.findAll)
+  .post(articlesController.create);
 
  router.route("/saved")
   .get(articlesController.findAll)
   .post(articlesController.create);
 
-//router
-//  .route("/:id")
-//  .get(articlesController.findById)
-//  .put(articlesController.update)
-//  .delete(articlesController.remove);
+router
+  .route("/:id")
+  .get(articlesController.findById)
+  .put(articlesController.update)
+  .delete(articlesController.remove);
 
  router
   .route("/saved/:id")
