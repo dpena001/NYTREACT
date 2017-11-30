0171d88c6330f3d42a335d5414340e0086697988
dpena001 <david.pena@vitcomp.com>
2017-11-30 05:09:40 -0500

    NTYREACT SEARCH HEROKU VERSION

--- server.js
+++ server.js
@@ -3,20 +3,30 @@ const express = require("express");
 const methodOverride = require("method-override");
 const bodyParser = require("body-parser");
 const path = require("path");
-const routes = require("./routes");
+const routes = require ('./routes');
+
+// Express configuration
 const app = express();
 
 const mongoose = require("mongoose");
+
 const PORT = process.env.PORT || 3001;
 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json());
+app.use(express.static('client/build'));
 
-app.use(express.static("client"));
+app.use(express.static('public'));
+app.use(methodOverride("_method"));
 
 // Add routes, both API and view
 app.use(routes);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, './client/public/index.html'));
+});
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nytreact";
 
